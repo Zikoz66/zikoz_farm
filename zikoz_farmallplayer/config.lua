a98237906a55a20fa79dd7923f8becc065fb07a5
Zikoz66 <45317500+Zikoz66@users.noreply.github.com>
2019-11-05 18:36:14 +0100

    Add files via upload

---
+++ zikoz_farmallplayer/config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.EnableBlips = true
+
+Config.Zones = {
+    { x = 354.65, y = 6517.87, z = 28.26},
+    { x = 2890.4, y = 4391.6, z = 50.3},
+    { x = -1500.24, y = -892.45, z = 10.107}
+}
+
+Config.Blip = {
+    Sprite  = 280,
+    Display = 4,
+    Scale   = 1.0,
+    Colour  = 0,
+    Text = "Circuit d'Orange"
+}
